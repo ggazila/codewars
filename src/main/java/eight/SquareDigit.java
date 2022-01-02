074df37f4a14992552576d8bcdc739a46bfbcdf7
Sergey Yevtushok <ggazila@gmail.com>
2022-01-02 16:59:25 +0200

    first commit

---
+++ src/main/java/eight/SquareDigit.java
@@ -0,0 +1,20 @@
+package eight;
+
+public class SquareDigit {
+
+    public int squareDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while (n > 0) {
+            int temp = n % 10;
+            sb.insert(0, temp * temp);
+            n /= 10;
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+
+}
---
+++ src/test/java/eight/SquareDigitTest.java
@@ -0,0 +1,14 @@
+package eight;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareDigitTest {
+    @Test
+    public void test() {
+        assertEquals(811181, new SquareDigit().squareDigits(9119));
+        assertEquals(0, new SquareDigit().squareDigits(0));
+    }
+
+}
