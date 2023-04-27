58050df895ad68c8d7256e0b03b9d4254918bc2d
Serhii Yevtushok <ggazila@gmail.com>
2023-04-27 07:20:38 +0300

    RunningOutOfSpace task

---
+++ src/main/java/seven/RunningOutOfSpace.java
@@ -0,0 +1,12 @@
+package seven;
+
+public class RunningOutOfSpace {
+    public static String[] spacey(String[] array) {
+        for (int i = 1; i < array.length; i++) {
+            array[i] = array[i - 1] + array[i];
+        }
+
+        return array;
+    }
+
+}
